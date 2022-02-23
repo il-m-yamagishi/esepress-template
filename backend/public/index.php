41546161b5a04bc4df18c650fc318f1ea6f30918
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2022-02-23 21:53:02 +0900

    chore: add tests directory

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$logger = (new App\Bootstrap\LoggerFactory())->createLogger('Semplice CLI');
+$logger = (new App\Bootstrap\LoggerFactory())->createLogger('Semplice HTTP');
 $request = \Laminas\Diactoros\ServerRequestFactory::fromGlobals();
 
 App\Bootstrap\HttpKernel::invoke($logger, $request);
